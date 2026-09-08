e00a7f1305a99f00f7c15a17bec573e114921ac5
Jonathan <jonathanesponja333@gmail.com>
2026-09-08 16:31:25 -0600

    Practica2: Primera app de flutter

---
+++ flutter_application_230318/presentation/screens/counter_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+
+class CounterScreen extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Counter Screen'),),
+    );
+  }
+}
